eeb11485624c2fb3ffa45ebc5c99d92b7792f4e0
Jeremy Landis <jeremylandis@hotmail.com>
2017-04-14 20:08:10 -0400

    [ci] Fix line endings / formatting

--- src/main/java/net/trajano/wagon/git/internal/AcceptAllHostKeyRepository.java
+++ src/main/java/net/trajano/wagon/git/internal/AcceptAllHostKeyRepository.java
@@ -4,55 +4,69 @@ import com.jcraft.jsch.HostKey;
 import com.jcraft.jsch.HostKeyRepository;
 import com.jcraft.jsch.UserInfo;
 
-public class AcceptAllHostKeyRepository implements HostKeyRepository {
+public class AcceptAllHostKeyRepository implements
+    HostKeyRepository {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public int check(String host, byte[] key) {
-		return HostKeyRepository.OK;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int check(String host,
+        byte[] key) {
 
-	/**
-	 * Does nothing. {@inheritDoc}
-	 */
-	@Override
-	public void add(HostKey hostkey, UserInfo ui) {
-	}
+        return HostKeyRepository.OK;
+    }
 
-	/**
-	 * Does nothing. {@inheritDoc}
-	 */
-	@Override
-	public void remove(String host, String type) {
-	}
+    /**
+     * Does nothing. {@inheritDoc}
+     */
+    @Override
+    public void add(HostKey hostkey,
+        UserInfo ui) {
 
-	/**
-	 * Does nothing. {@inheritDoc}
-	 */
-	@Override
-	public void remove(String host, String type, byte[] key) {
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @return the class name
-	 */
-	@Override
-	public String getKnownHostsRepositoryID() {
-		return this.getClass().getName();
-	}
+    /**
+     * Does nothing. {@inheritDoc}
+     */
+    @Override
+    public void remove(String host,
+        String type) {
 
-	@Override
-	public HostKey[] getHostKey() {
-		return new HostKey[0];
-	}
+    }
 
-	@Override
-	public HostKey[] getHostKey(String host, String type) {
-		return new HostKey[0];
-	}
+    /**
+     * Does nothing. {@inheritDoc}
+     */
+    @Override
+    public void remove(String host,
+        String type,
+        byte[] key) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return the class name
+     */
+    @Override
+    public String getKnownHostsRepositoryID() {
+
+        return this.getClass().getName();
+    }
+
+    @Override
+    public HostKey[] getHostKey() {
+
+        return new HostKey[0];
+    }
+
+    @Override
+    public HostKey[] getHostKey(String host,
+        String type) {
+
+        return new HostKey[0];
+    }
 
 }
